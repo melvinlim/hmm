c2db831bbecba2657cfc30522b96c21e772065e4
Melvin Lim <melvinlim@MacBook.local>
2018-10-23 19:04:22 -0700

    implement viterbi algorithm

--- hmm.py
+++ hmm.py
@@ -16,9 +16,11 @@ class HMM:
 		self.pi=array(N,tmp)
 		self.alpha=matrix(T,N)
 		self.beta=matrix(T,N)
+		self.delta=matrix(T,N)
+		self.psi=matrix(T,N)
 	def forward(self,obs):
 		for i in xrange(self.N):
-			self.alpha[1][i]=self.pi[i]*self.B[i][obs[1]]
+			self.alpha[0][i]=self.pi[i]*self.B[i][obs[0]]
 		for t in xrange(self.T-1):
 			tmp=0
 			for j in xrange(self.N):
@@ -34,3 +36,18 @@ class HMM:
 				for j in xrange(self.N):
 					tmp+=self.A[i][j]*self.B[j][obs[t+1]]*self.beta[t+1][j]
 				self.beta[t][i]=tmp
+	def viterbi(self):
+		for i in xrange(self.N):
+			self.delta[0][i]=self.pi[i]*self.B[i][obs[0]]
+			self.psi[0][i]=0
+		for t in xrange(1,self.T):
+			maxVal=0
+			maxArg=0
+			for j in xrange(self.N):
+				for i in xrange(self.N):
+					tmp=self.delta[t-1][i]*self.A[i][j]
+					if tmp>maxVal:
+						maxVal=tmp
+						maxArg=i
+				self.delta[t][j]=maxVal*self.B[j][obs[t]]
+				self.psi[t][j]=maxArg
