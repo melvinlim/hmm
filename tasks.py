401f1e5adb7fb4e89a14a6602156096a73246933
Melvin Lim <melvinlim@MacBook.local>
2018-10-27 16:45:41 -0700

    sample noise from uniform distribution

--- tasks.py
+++ tasks.py
@@ -33,15 +33,15 @@ class JarTask(Task):
 	def getNoisy(self,nObs,mean,var,trueObs,noisyObs):
 		for o in xrange(nObs):
 			trueObs.append(self.draw())
-			noisyObs.append(self.draw()+self.noise(mean,var))
+			noisyObs.append(self.draw()+self.noise(var))
 	def getSingleNoisy(self,mean,var,trueObs,noisyObs):
 		trueObs.pop(0)
 		noisyObs.pop(0)
 		trueObs.append(self.draw())
-		noisyObs.append(self.draw()+self.noise(mean,var))
-	def noise(self,mean,var):
+		noisyObs.append(self.draw()+self.noise(var))
+	def noise(self,var):
 		if var==0:
 			return 0
 		x=random.randint(0,1000)/1000.0
-		n=math.exp(-0.5*(x-0.5)**2/var)/(2*math.pi*var)
-		return n-0.5
+#		n=math.exp(-0.5*(x-0.5)**2/var)/(2*math.pi*var)
+		return x-0.5
